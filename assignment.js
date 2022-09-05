1e2fd1caaa4cc8b8a638ac04e9f31c335a329430
ojibaroland <ojibaroland123@gmail.com>
2022-09-05 16:57:57 +0100

    move

--- assignment.js
+++ assignment.js
@@ -1,8 +1,14 @@
 const randUser = async() =>{
-    let Data = await fetch("https://randomuser.me/api")
-     let convertedData = await Data.json();
-     let output = convertedData.results;
+    // fetching data
+    let dataNeeded= await fetch("https://randomuser.me/api")
+
+    // converting to json
+     let jsonData = await dataNeeded.json();
+
+     let output = jsonData.results;
+
      let result = output[0]
+     
      let finalResult = `<img src="${result.picture.large}"><br> Full name: ${result.name.title} ${result.name.first} ${result.name.last} <br> Home: ${result.location.street.number}, ${result.location.street.name} <br> State: ${result.location.state} <br> Country: ${result.location.country} <br> E-mail: ${result.email} <br> DOB: ${result.dob.date}<br> Age: ${result.dob.age}<br> Phone: ${result.phone}`
       return document.body.innerHTML = finalResult
     
